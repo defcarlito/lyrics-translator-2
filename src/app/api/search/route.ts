84685456226add3ee2fb3e4ee9cb1c60dec9aada
defcarlito <harrisoncamorim@gmail.com>
2025-08-06 12:41:58 -0500

    basic genius api setup

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,11 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server"
 
 export async function POST(request: NextRequest) {
   const body = await request.json()
-
   const search = body.search
 
+  const accessToken = process.env.GENIUS_ACCESS_TOKEN
+
+  const url = `https://api.genius.com/search?q=${encodeURIComponent(search)}`
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+
+  const data = await response.json()
+  const topHit = data.response.hits[0]
+
   return NextResponse.json({
-    message: `You searched for "${search}"`,
+    topHit,
   })
 }
