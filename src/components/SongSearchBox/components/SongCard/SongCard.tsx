fdeae70aabfca1f5466a986e8dbd87b8249a2104
defcarlito <harrisoncamorim@gmail.com>
2025-08-07 15:15:50 -0500

    loading state

--- src/components/SongSearchBox/components/SongCard/SongCard.tsx
+++ src/components/SongSearchBox/components/SongCard/SongCard.tsx
@@ -2,11 +2,19 @@ import { Song } from "@/types/song"
 
 export default function SongCard({ song }: { song: Song }) {
   return (
-    <div className="flex gap-4 hover:bg-accent rounded-md transition">
+    <div
+      className="flex gap-4 hover:bg-accent rounded-md transition"
+      role="button"
+      tabIndex={0}
+    >
       <div className="shrink-0">
-        <img src={song.albumCover} alt="cover" className="size-32 rounded-md shadow-sm" />
+        <img
+          src={song.albumCover}
+          alt="cover"
+          className="size-32 rounded-md shadow-sm"
+        />
       </div>
-      <div className="flex flex-col justify-center items-start">
+      <div className="flex flex-col justify-center items-start gap-1">
         <h1 className="font-bold">{song.title}</h1>
         <p className="text-sm text-wrap">by {song.artist}</p>
       </div>
