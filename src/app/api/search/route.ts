d5193482706237f3aa5c8f15f195f0c2900d31df
defcarlito <harrisoncamorim@gmail.com>
2025-08-07 12:01:50 -0500

    genius endpoint to fetch songs

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,4 +1,7 @@
+import { Song } from "@/types/song"
 import { NextRequest, NextResponse } from "next/server"
+
+const RESULT_LIMIT = 3
 
 export async function POST(request: NextRequest) {
   const body = await request.json()
@@ -14,9 +17,45 @@ export async function POST(request: NextRequest) {
   })
 
   const data = await response.json()
-  const topHit = data.response.hits[0]
+  const getTopHits = () => {
+    const totalHits: number = data.response.hits.length
+
+    // limit # search results to < RESULT_LIMIT
+    const totalSearchResult: number = Math.min(RESULT_LIMIT, totalHits)
+    const topSearchResults: JSON[] = data.response.hits.slice(
+      0,
+      totalSearchResult
+    )
+
+    return topSearchResults
+  }
+
+  const searchHits = getTopHits()
+
+  const formattedSongInfo: Song[] = searchHits.map((hit: any) => {
+    const songInfo = hit.result
+
+    const getFeaturedArtistNames = () => {
+      const allFeaturedArtists: JSON[] = songInfo.featured_artists
+      const allFeaturedArtistsNames: string[] = allFeaturedArtists.map(
+        (artist: any) => {
+          return artist.name
+        }
+      )
+      return allFeaturedArtistsNames
+    }
+
+    const featuredArtistsNames = getFeaturedArtistNames()
+
+    return {
+      title: songInfo.title,
+      artist: songInfo.primary_artist.name,
+      featuredArtists: featuredArtistsNames,
+      albumCover: songInfo.header_img_url,
+    }
+  })
 
   return NextResponse.json({
-    topHit,
+    songs: formattedSongInfo,
   })
 }
