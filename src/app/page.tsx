21fd2abb070fe4cf3e4de893259201ed0904f2be
defcarlito <harrisoncamorim@gmail.com>
2025-08-08 13:56:33 -0500

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Content from "@/components/content/Content.client"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen">
+    <div className="flex flex-col items-center justify-center h-screen w-screen relative">
       <Content />
     </div>
   )
