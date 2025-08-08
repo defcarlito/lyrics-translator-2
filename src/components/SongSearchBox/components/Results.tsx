f99d86321aac986f2d6fe29c41fe93d168cc2a85
defcarlito <harrisoncamorim@gmail.com>
2025-08-08 09:59:00 -0500

    fixed loading skeleton issue

--- src/components/SongSearchBox/components/Results.tsx
+++ src/components/SongSearchBox/components/Results.tsx
@@ -10,9 +10,9 @@ export default function Results({
   loading: boolean
 }) {
   return (
-    <div className="space-y-4 border p-4 w-md rounded-lg bg-card shadow-sm">
+    <div className="flex flex-col justify-between border p-4 w-md h-128 rounded-lg bg-card shadow-sm">
       {loading
-        ? songInfo.map((_, index) => <LoadingCard key={index} />)
+        ? Array.from({ length: 3 }).map((_, index) => <LoadingCard key={index} />)
         : songInfo.map((song, index) => <SongCard song={song} key={index} />)}
     </div>
   )
