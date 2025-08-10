83ca749048008e85b5b96183f49c09b8241f9a9f
defcarlito <harrisoncamorim@gmail.com>
2025-08-10 18:18:04 -0500

    init lyrics endpoint

---
+++ src/app/api/get-lyrics/route.ts
@@ -0,0 +1,39 @@
+import { Lyrics } from "@/types/lyrics"
+import { Song } from "@/types/song"
+import Genius from "genius-lyrics"
+import { NextRequest, NextResponse } from "next/server"
+
+const Client = new Genius.Client(process.env.GENIUS_ACCESS_TOKEN)
+
+export async function POST(request: NextRequest) {
+  const body = await request.json()
+  const song: Song = body.song
+
+  const searches = await Client.songs.search(`${song.title} by ${song.artist}`)
+  const topSearchLyrics: string = await searches[0].lyrics(true)
+
+  const lyricsByLine = topSearchLyrics.split("\n")
+  const formatLines = () => {
+    lyricsByLine.shift() // Remove contributors line
+    while (lyricsByLine[0] == "") {
+      // Remove empty lines in the start
+      lyricsByLine.shift()
+    }
+  }
+  formatLines()
+
+  const lyricsByWord = lyricsByLine.flatMap((sentence) =>
+    sentence.split(" ").filter((word) => word !== "")
+  )
+
+  console.log(topSearchLyrics)
+  console.log("---")
+  console.log(lyricsByLine)
+  console.log("---")
+  console.log(lyricsByWord)
+
+  return NextResponse.json({
+    byLine: lyricsByLine,
+    byWord: lyricsByWord,
+  })
+}
