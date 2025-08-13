3be81e83231ced68d21e6accda17191df082697e
defcarlito <harrisoncamorim@gmail.com>
2025-08-13 12:00:02 -0500

    init firebase config

--- src/app/api/translate-word/route.ts
+++ src/app/api/translate-word/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server"
 export async function POST(request: NextRequest) {
   const body = await request.json()
 
+
   return NextResponse.json({
     word: "test",
   })
--- src/components/content/LyricsVocabBox/components/VocabWordsBox/VocabWordsBox.tsx
+++ src/components/content/LyricsVocabBox/components/VocabWordsBox/VocabWordsBox.tsx
@@ -10,8 +10,8 @@ export default function VocabWordsBox({
 }) {
   return (
     <div className="bg-card border p-4 rounded-md w-md grid grid-cols-3 gap-4 content-start">
-      {[...clickedWords].map((word) => (
-        <WordCard word={word} />
+      {[...clickedWords].map((word, index) => (
+        <WordCard word={word} key={index} />
       ))}
     </div>
   )
