bde880070dba8bc5dd37d2610ecc7bdeb60a1e95
defcarlito <harrisoncamorim@gmail.com>
2025-08-13 12:56:24 -0500

    basic fetch for translation

--- src/components/content/LyricsVocabBox/components/VocabWordsBox/components/WordCard.tsx
+++ src/components/content/LyricsVocabBox/components/VocabWordsBox/components/WordCard.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react"
+
 export default function WordCard({ word }: { word: string }) {
+  useEffect(() => {
+    fetch("/api/translate-word", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ word: word }),
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  }, [])
+
   return (
     <div className="bg-card p-4 border rounded-sm">
       <h1>{word}</h1>
