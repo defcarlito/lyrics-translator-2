2787657547436ad01684da0c9e597f6796fb88df
defcarlito <harrisoncamorim@gmail.com>
2025-08-14 12:33:36 -0500

    formatted model responses and writes to fb cache

--- src/app/api/translate-word/route.ts
+++ src/app/api/translate-word/route.ts
@@ -3,6 +3,9 @@ import OpenAI from "openai"
 import { zodTextFormat } from "openai/helpers/zod.mjs"
 import { z } from "zod"
 
+import { db } from "@/app/firebase/config"
+import { doc, setDoc } from "firebase/firestore"
+
 const PARTS_OF_SPEECH = [
   "noun",
   "verb",
@@ -18,33 +21,77 @@ const PARTS_OF_SPEECH = [
   "proper noun",
 ] as const
 
+const LANGUAGES = ["portuguese", "english", "other"] as const
+
 const WORD_GENDERS = ["male", "female", "both"] as const
 
 export async function POST(request: NextRequest) {
   const body = await request.json()
-  const word = toLowerAndStripPunctuation(body.word)
+  const untranslatedWord = body.word
+  
+  const obj = await createTranslation(untranslatedWord)
+  await writeToTranslationCache(obj)
+
+  return NextResponse.json({
+    data: obj,
+  })
+}
+
+async function createTranslation(untranslatedWord: string) {
+  const word = toLowerAndStripPunctuation(untranslatedWord)
 
   const wordMetaData = z.object({
     word: z.literal(word),
-    translation: z
-      .string()
-      .describe("A succinct, short English translation of the word."),
-    pos: z.enum(PARTS_OF_SPEECH).describe("The word's part of speech."),
-    gender: z.enum(WORD_GENDERS).optional().nullable(),
-    infitive: z
-      .string()
-      .optional()
-      .nullable()
-      .describe("The infinitive form if the word is a verb."),
+    meanings: z
+      .array(
+        z
+          .object({
+            translation: z
+              .string()
+              .describe(
+                "A succinct, short, natural English translation of the word with no unnecessary punctuation."
+              ),
+            pos: z
+              .enum(PARTS_OF_SPEECH)
+              .describe("The meaning's part of speech."),
+            gender: z.enum(WORD_GENDERS).optional().nullable(),
+            infinitive: z
+              .string()
+              .optional()
+              .nullable()
+              .describe("The infinitive form if the word is a verb."),
+            slang: z
+              .boolean()
+              .describe(
+                "Indicates whether this meaning is considered slang in everyday usage."
+              ),
+          })
+          .nullable()
+          .describe(
+            "Set to null only if the word is not a valid Brazilian Portuguese word."
+          )
+      )
+      .max(2, "A limit of 2 distinct words."),
+    language: z.enum(LANGUAGES).describe("Which language the word belongs to."),
   })
 
   const client = new OpenAI()
+
   const response = await client.responses.parse({
     model: "gpt-5-mini",
     input: [
       {
         role: "system",
-        content: `You are a Brazilian Portuguese linguist teaching Portuguese. Extract metadata for the word ${word} in the given structure.`,
+        content: "You are a Brazilian Portuguese linguist teaching Portuguese.",
+      },
+      {
+        role: "user",
+        content: `
+        Extract only the most clearly distinct and commonly used meanings for the word ${word} and 
+        metadata in the given structure. The word may or may not have multiple meanings across 
+        different parts of speech - Do not confuse words with different spellings or accents as variants 
+        of the same word. Avoid any explanatory text in parentheses (i.e. "(auxiliary)", "(colloquial)", etc). 
+        `,
       },
     ],
     text: {
@@ -54,14 +101,36 @@ export async function POST(request: NextRequest) {
 
   const event = response.output_parsed
 
-  console.log(event)
-  console.log(response.usage)
+  const eventFormatted = {
+    word: event?.word,
+    meanings: event?.meanings.map((meaning) => ({
+      ...meaning,
+      infinitive: meaning?.infinitive
+        ? toLowerAndStripPunctuation(meaning.infinitive)
+        : null,
+    })),
+    language: event?.language,
+  }
 
-  return NextResponse.json({
-    data: event,
+  console.log(eventFormatted)
+  console.log(response.usage)
+  return eventFormatted
+}
+
+function checkTranslationCache(untranslatedWord: string): boolean {
+  return false
+}
+
+async function writeToTranslationCache(obj: any) {
+  const word = obj.word
+  const meanings = obj.meanings
+  const language = obj.language
+
+  await setDoc(doc(db, language, word), {
+    meanings,
   })
 }
 
 function toLowerAndStripPunctuation(str: string) {
-  return str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase()
+  return str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "").toLowerCase()
 }
