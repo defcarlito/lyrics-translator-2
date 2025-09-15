89d1662d83149bca63e56eae827940690c7a930d
defcarlito <harrisoncamorim@gmail.com>
2025-09-14 22:12:51 -0500

    added word type

--- src/types/word.ts
+++ src/types/word.ts
@@ -1,7 +1,29 @@
 export type Word = {
-  inTarget: string
-  inEnglish: string
-  POS: string
-  gender: "male" | "female"
-  infinitive: string | undefined
+  word: string
+  meanings: Meaning[]
+  language: Language
 }
+
+export type Meaning = {
+  translation: string
+  pos: POS
+  gender: "male" | "female" | "both"
+  infinitive: string | null
+  slang: boolean
+}
+
+type POS = 
+  | "noun"
+  | "verb"
+  | "article"
+  | "adjective"
+  | "pronoun"
+  | "adverb"
+  | "conjuction"
+  | "preposition"
+  | "interjection"
+  | "contraction"
+  | "numeral"
+  | "proper noun"
+
+  export type Language = "portuguese" | "english" | "other"
