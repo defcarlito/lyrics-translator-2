c072de9f0a5ecd14aed75d0691e5655cf2277dab
defcarlito <harrisoncamorim@gmail.com>
2025-09-14 22:21:25 -0500

    changed fetch to accept word type instead of json obj

--- src/components/content/LyricsVocabBox/components/VocabWordsBox/components/WordCard.tsx
+++ src/components/content/LyricsVocabBox/components/VocabWordsBox/components/WordCard.tsx
@@ -1,4 +1,28 @@
+import { Word } from "@/types/word"
 import { useQuery } from "@tanstack/react-query"
+import { useEffect, useState } from "react"
+
+
+export default function WordCard({ word }: { word: string }) {
+  const [metadata, setMetadata] = useState<Word | undefined>(undefined)
+  
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["wordTranslation", word],
+    queryFn: () => fetchWordTranslation(word),
+    staleTime: 1000 * 60 * 5,
+  })
+  
+  useEffect(() => {
+    if (data?.data) setMetadata(data.data)
+    }, [data])
+  
+  return (
+    <div className="bg-card p-4 border rounded-sm">
+      <h1>{toLowerAndStripPunctuation(word)}</h1>
+      <p>{isLoading ? "Loading..." : metadata?.meanings.at(0)?.translation}</p>
+    </div>
+  )
+}
 
 async function fetchWordTranslation(word: string) {
   const result = await fetch("/api/translate-word", {
@@ -12,17 +36,6 @@ async function fetchWordTranslation(word: string) {
   return result.json()
 }
 
-export default function WordCard({ word }: { word: string }) {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["wordTranslation", word],
-    queryFn: () => fetchWordTranslation(word),
-    staleTime: 1000 * 60 * 5,
-  })
-
-  return (
-    <div className="bg-card p-4 border rounded-sm">
-      <h1>{word}</h1>
-      <p>description</p>
-    </div>
-  )
+function toLowerAndStripPunctuation(str: string) {
+  return str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "").toLowerCase()
 }
