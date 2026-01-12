cac802022176277f1b9e2c43fb065cacd41076c1
harrisonamorim <harrisoncamorim@gmail.com>
2026-01-11 20:23:47 -0600

    styling

--- src/components/content/LyricsVocabBox/components/VocabWordsBox/components/WordCard/components/MetadataSkeleton.tsx
+++ src/components/content/LyricsVocabBox/components/VocabWordsBox/components/WordCard/components/MetadataSkeleton.tsx
@@ -1,11 +1,13 @@
 export default function MetadataSkeleton({ word }: { word: string }) {
   return (
-    <div className="flex flex-col bg-card p-4 border rounded-sm gap-1 items-center">
-      <h1>{word}</h1>
+    <div className="flex flex-col bg-card p-2 border rounded-sm gap-1 items-start">
+      <h1 className="border-b-1 w-full">
+        <i>{word}</i>
+      </h1>
       <p className="bg-accent text-transparent w-full rounded-md">definition</p>
-      <div className="flex justify-between w-full">
-        <p className="bg-accent text-transparent w-fit rounded-md">badge</p>
-        <p className="bg-accent text-transparent w-fit rounded-md">badge</p>
+      <div className="flex justify-between w-full gap-1">
+        <p className="bg-accent text-transparent w-full rounded-md">badge</p>
+        <p className="bg-accent text-transparent w-full rounded-md">badge</p>
       </div>
     </div>
   )
--- src/components/content/SongSearchBox/components/SongSkeleton/SongSkeleton.tsx
+++ src/components/content/SongSearchBox/components/SongSkeleton/SongSkeleton.tsx
@@ -5,8 +5,12 @@ export default function SongSkeleton() {
         <div className="box size-36 bg-accent rounded-md opacity-50"></div>
       </div>
       <div className="flex flex-col justify-center items-start gap-1">
-        <h1 className="bg-accent text-transparent rounded-md opacity-50">super long loading title</h1>
-        <p className="text-sm bg-accent text-transparent rounded-md opacity-50">by loading artist</p>
+        <h1 className="bg-accent text-transparent rounded-md opacity-50">
+          super long loading title
+        </h1>
+        <p className="text-sm bg-accent text-transparent rounded-md opacity-50">
+          by loading artist
+        </p>
       </div>
     </div>
   )
