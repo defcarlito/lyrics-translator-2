a1cbb0fa777b01b8b0de5f89b288dde0f15d0f9d
harrisonamorim <harrisoncamorim@gmail.com>
2026-01-11 20:21:25 -0600

    setup toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ReactQueryProvider>{children}</ReactQueryProvider>
+        <Toaster />
       </body>
     </html>
   )
