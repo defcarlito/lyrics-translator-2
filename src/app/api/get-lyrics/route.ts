d00bc139ae83e67ce1a7d1d9b300f2cb9f4a7a8c
harrisonamorim <harrisoncamorim@gmail.com>
2026-01-13 15:30:46 -0600

    migrated lyric fetcher from genius to lrclib

--- src/app/api/get-lyrics/route.ts
+++ src/app/api/get-lyrics/route.ts
@@ -1,29 +1,21 @@
 export const runtime = "nodejs"
 
 import { Song } from "@/types/song"
-import { Client } from "genius-lyrics"
 import { NextRequest, NextResponse } from "next/server"
 
-const REMOVE_CHORUS = true
-
 export async function POST(request: NextRequest) {
-  const client = new Client(process.env.GENIUS_ACCESS_TOKEN)
-
   const body = await request.json()
   const song: Song = body.song
 
-  const searches = await client.songs.search(`${song.title} by ${song.artist}`)
-  const topSearchLyrics: string = await searches[0].lyrics(REMOVE_CHORUS)
+  const s = await fetch(
+    `https://lrclib.net/api/search?track_name=${encodeURIComponent(song.title)}&artist_name=${encodeURIComponent(song.artist)}`,
+  ).then((r) => r.json())
 
+  const topSearchLyrics = s[0].plainLyrics
   const lyricsByLine = topSearchLyrics.split("\n")
-  lyricsByLine.shift() // Remove contributors line
-  while (lyricsByLine[0] == "") {
-    // Remove empty lines in the start
-    lyricsByLine.shift()
-  }
 
-  const lyricsByWord = lyricsByLine.flatMap((sentence) =>
-    sentence.split(" ").filter((word) => word !== ""),
+  const lyricsByWord = lyricsByLine.flatMap((sentence: string) =>
+    sentence.split(" ").filter((word: string) => word !== ""),
   )
 
   return NextResponse.json({
